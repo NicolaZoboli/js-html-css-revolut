9579ba5c3964ead1db057b04d0d905fe986a54e8
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-07-08 11:18:22 +0200

    aggiunto hamburger menu e il suo dropdown con jquery

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
   },
   function() {
     $(this).children('.dropdown').hide();
-  }
-);
+  });
+
+$('.show-hamburger-dropdown').click(function() {
+  $(this).next('#hamburger-dropdown').show(),
+  $(this).hide();
+  });
+
+$('.hide-hamburger-dropdown').click(function() {
+  $(this).parents('#hamburger-dropdown').hide(),
+  $(this).parents('#hamburger-dropdown').prev('.show-hamburger-dropdown').show();
+  });
 });
