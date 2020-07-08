ca7b54df56751496fe18d85b08e23b308c979a1e
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-07-08 13:36:42 +0200

    corretta la funzione che gestisce il dropdown e corretto il posizionamento html del dropdown

--- js/script.js
+++ js/script.js
@@ -7,12 +7,29 @@ $(document).ready(function() {
   });
 
 $('.show-hamburger-dropdown').click(function() {
-  $(this).next('#hamburger-dropdown').show(),
+  $(this).parents('#main-nav').next('#hamburger-dropdown').show(),
   $(this).hide();
+  $('#main-nav').hide();
+  $('#jumbo').hide();
+  $('main').removeClass('padding-generale');
   });
 
 $('.hide-hamburger-dropdown').click(function() {
   $(this).parents('#hamburger-dropdown').hide(),
-  $(this).parents('#hamburger-dropdown').prev('.show-hamburger-dropdown').show();
+  $(this).parents('#hamburger-dropdown').prev('#main-nav').children('.show-hamburger-dropdown').show();
+  $('#main-nav').show();
+  $('#jumbo').show();
+  $('main').addClass('padding-generale');
   });
+});
+
+$(window).resize(function(){
+  if ($('header').width() > 767){
+         $('#hamburger-dropdown').hide();
+         $('#main-nav').show();
+         $('#jumbo').show();
+         $('main').addClass('padding-generale');
+  } else if ($('header').width() < 767) {
+    $('.show-hamburger-dropdown').show();
+  }
 });
